b564fa98ccc0bfd6f9554f00f71f848926bc0c27
Héctor Esmoar <50776035+hesmoar@users.noreply.github.com>
2025-03-13 14:33:32 -0700

    Added support to request user to select folders with GUI

--- davinci_publisher_standalone.py
+++ davinci_publisher_standalone.py
@@ -2,10 +2,30 @@
 import os
 import sys
 import pprint
+import tkinter as tk
+from tkinter import filedialog
 
 
-EXPORT_DIRECTORY = r"D:\HecberryStuff\PAINANI STUDIOS\1_Proyectos\Active\1_Animaorquesta\PipeTest"
-OUTPUT_FOLDER = r"D:\HecberryStuff\PAINANI STUDIOS\1_Proyectos\Active\1_Animaorquesta\PipeTest\RenderTest\Clips"
+#EXPORT_DIRECTORY = r"D:\HecberryStuff\PAINANI STUDIOS\1_Proyectos\Active\1_Animaorquesta\PipeTest"
+#OUTPUT_FOLDER = r"D:\HecberryStuff\PAINANI STUDIOS\1_Proyectos\Active\1_Animaorquesta\PipeTest\RenderTest\Clips"
+
+
+def ask_folders():
+    root = tk.Tk()
+    root.withdraw()  # Hide the main window
+
+    root.lift()  # Bring the window to the front
+    root.attributes("-topmost", 1)  # Keep the window on top
+    
+    output_folder = filedialog.askdirectory(title="Select Render Output Folder")
+    export_folder = filedialog.askdirectory(title="Select edl Output Folder") 
+    if not output_folder or not export_folder:
+        print("Error: Both folders must be selected.")
+        sys.exit(1)  # Exit the script if folders are not selected
+
+    return export_folder, output_folder
+EXPORT_DIRECTORY, OUTPUT_FOLDER = ask_folders()
+print(EXPORT_DIRECTORY, OUTPUT_FOLDER)
 
 def get_current_project():
 
@@ -71,6 +91,11 @@ def export_edl():
     if not project:
         print("No current project found")
         return False
+    
+    timeline = project.GetCurrentTimeline()
+    if not timeline:
+        print("No current timeline found")
+        return
     
     edl_name = get_timeline_name(project)
     if not edl_name:

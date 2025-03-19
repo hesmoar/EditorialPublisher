c233cdbf39cbea58ee542bc3707d403d2c5436de
Héctor Esmoar <50776035+hesmoar@users.noreply.github.com>
2025-03-19 16:23:07 -0700

    Added tkinter gui to select the path of the otio or edl file

--- kitsu_editorial_publisher.py
+++ kitsu_editorial_publisher.py
@@ -3,18 +3,26 @@ import pprint
 import json
 import opentimelineio as otio
 import re
+import tkinter as tk
 from kitsu_project_context import get_project, select_project
+from tkinter import filedialog
 
 
-test_edl = r"D:\HecberryStuff\PAINANI STUDIOS\1_Proyectos\Active\1_Animaorquesta\PipeTest\AO_PipeTest_A_v009.edl"
 
-#regex_pattern = r"(AO)_(\d{4})-(\d{4})"
 regex_pattern = r"(\w+)_(\d{4})-(\d{4})"
 
+def ask_for_file_path():
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename(
+        title="Select an EDL or OTIO file",
+        filetypes=(("EDL files", "*.edl"),("OTIO files", "*.otio"),("All files", "*.*"))
+    )
+    return file_path
 
 #This file will compare the info from the EDL with the information in Kitsu and update based on that. 
 
-def read_edl():
+def read_edl(file_path):
     """
     Reads an EDL (Edit Decision List) file and extracts shot information.
     This function reads an EDL file using the OpenTimelineIO (otio) library,
@@ -27,30 +35,66 @@ def read_edl():
             - timeframe_in (str): The start timecode of the shot.
             - timeframe_out (str): The end timecode of the shot.
     """
-    timeline = otio.adapters.read_from_file(test_edl)
+    timeline = otio.adapters.read_from_file(file_path)
     edl_shots = []
 
     for track in timeline.tracks:
         for clip in track:
-            clip_name = clip.name
-            clip_range = clip.range_in_parent()
-            clip_in = clip_range.start_time.to_timecode()
-            clip_out = clip_range.end_time_inclusive().to_timecode()
+            if isinstance(clip, otio.schema.Clip):
+                clip_name = clip.name
+                clip_range = clip.range_in_parent()
 
-            match = re.match(regex_pattern, clip_name)
-            if match:
-                shot_name = match.group(3)
-                edl_shots.append({"name": shot_name, 
-                                  "timeframe_in": clip_in, 
-                                  "timeframe_out": clip_out
-                                  })
+                if clip_range:
+                    start_time = clip_range.start_time
+                    duration = clip_range.duration
+                    frame_rate = start_time.rate
+
+                    clip_in = start_time.to_timecode()
+                    end_time = start_time + duration - otio.opentime.RationalTime(1, frame_rate)
+                    clip_out = end_time.to_timecode()
+
+                    match = re.match(regex_pattern, clip_name)
+                    if match:
+                        shot_name = match.group(3)
+                        edl_shots.append({"name": shot_name, 
+                                          "timeframe_in": clip_in, 
+                                          "timeframe_out": clip_out
+                                          })
                 #print(f"Match found for clip {clip_name}")
-            else:
-                print(f"No match found for clip {clip_name}")
+                    else:
+                        print(f"No match found for clip {clip_name}")
     return edl_shots
 
+def read_otio(file_path):
+    timeline = otio.adapters.read_from_file(file_path)
+    otio_shots = []
 
- 
+    for track in timeline.tracks:
+        for clip in track:
+            if isinstance(clip, otio.schema.Clip):   # Ensure it's a clip
+                clip_timein_source = clip.source_range.start_time
+                clip_end_time = clip.source_range.end_time_exclusive() - otio.opentime.RationalTime(1, clip_timein_source.rate)
+
+
+                
+                #one_frame = otio.opentime.RationalTime(1, clip_timein_source.rate)
+                #clip_timeout_sum = clip_timein_source + clip_duration - one_frame
+                #print(clip_timeout_sum)
+                clip_timein = clip_timein_source.to_timecode()
+                clip_timeout = clip_end_time.to_timecode()
+                print(f"Clip: {clip.name}, Start Time: {clip_timein}, Duration: {clip_timeout}")
+
+                match = re.match(regex_pattern, clip.name)
+                if match:
+                    shot_name = match.group(3)
+                    otio_shots.append({"name": shot_name,
+                                       "timeframe_in": clip_timein,
+                                       "timeframe_out": clip_timeout
+                                       })
+                else:
+                    print(f"No match found for clip {clip.name}")
+    #pprint.pprint(otio_shots)
+    return otio_shots
 
 
 #This function gets all the shots from a project in kitsu and creates a dictionarywith their name, timeframe_in and timeframe_out keys
@@ -82,15 +126,16 @@ def get_project_shots():
 
 
 # This function compares the dictionary coming from the edl and the one from kitsu to see if the shot names are matching. 
-def compare_shots():
-    kitsu_shots = get_project_shots()
-    edl_shots = read_edl()
+def compare_shots(edit_shots, kitsu_shots):
+
+    
+
     shots_to_update = []
 
-    for edl_shot in edl_shots:
-        edl_shot_name = edl_shot.get("name")
-        edl_shot_cut_in = edl_shot.get("timeframe_in")
-        edl_shot_cut_out = edl_shot.get("timeframe_out")
+    for shot in edit_shots:
+        shot_name = shot.get("name")
+        shot_cut_in = shot.get("timeframe_in")
+        shot_cut_out = shot.get("timeframe_out")
         
 
         match_found = False
@@ -102,23 +147,24 @@ def compare_shots():
             kitsu_shot_id = kitsu_shot.get("id")
 
 
-            if edl_shot_name == kitsu_shot_name:
-                if edl_shot_cut_in != kitsu_shot.get("timeframe_in"):
+            if shot_name == kitsu_shot_name:
+                if shot_cut_in != kitsu_shot.get("timeframe_in"):
+                    print(f"Cut in for shot {shot_name} is different in Kitsu")
                     shots_to_update.append({
-                        "name": edl_shot_name,
-                        "timeframe_in": edl_shot_cut_in,
-                        "timeframe_out": edl_shot_cut_out,
+                        "name": shot_name,
+                        "timeframe_in": shot_cut_in,
+                        "timeframe_out": shot_cut_out,
                         "id": kitsu_shot_id
                         })
-                    print(f"Shot {edl_shot_name} will be updated in Kitsu")
+                    print(f"Shot {shot_name} will be updated in Kitsu")
 
                     match_found = True
-                if edl_shot_cut_out != kitsu_shot.get("timeframe_out"):
-                    print(f"Cut out for shot {edl_shot_name} is different in Kitsu")
+                if shot_cut_out != kitsu_shot.get("timeframe_out"):
+                    print(f"Cut out for shot {shot_name} is different in Kitsu")
                     shots_to_update.append({
-                        "name": edl_shot_name,
-                        "timeframe_in": edl_shot_cut_in,
-                        "timeframe_out": edl_shot_cut_out,
+                        "name": shot_name,
+                        "timeframe_in": shot_cut_in,
+                        "timeframe_out": shot_cut_out,
                         "id": kitsu_shot_id
                         })
                     match_found = True
@@ -127,8 +173,18 @@ def compare_shots():
 
 
 def update_kitsu():
-    #project = get_project()
-    shots_to_update = compare_shots()
+    kitsu_shots = get_project_shots()
+    file_path = ask_for_file_path()
+
+    if file_path.endswith('.edl'):
+        edit_shots = read_edl(file_path)
+    elif file_path.endswith('.otio'):
+        edit_shots = read_otio(file_path)
+    else:
+        print("Unsupported file format")
+        return
+
+    shots_to_update = compare_shots(edit_shots, kitsu_shots)
 
     if not shots_to_update:
         print("No shots to update. Everything is up to date.")
